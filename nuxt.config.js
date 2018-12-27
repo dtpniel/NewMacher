5f78a3d09996dafd49680bcec979f5436e1363ff
Diti Pniel <dtpozner@gmail.com>
2018-12-27 11:30:08 +0200

    Change static files location

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,11 @@
 const pkg = require('./package')
+import webpack from 'webpack'
 
 module.exports = {
   mode: 'universal',
-
+  router: {
+    base: '/'
+  },
   /*
   ** Headers of the page
   */
@@ -14,8 +17,25 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+
+      { rel: 'stylesheet', href: '/css/style.css' },
+      { rel: 'stylesheet', href: '/css/colors/blue.css' }
+
+    ],
+    // ,
+    script: [
+      { src: '/js/jquery-3.3.1.min.js' },
+      { src: '/js/jquery-migrate-3.0.0.min.js' },
+      { src: '/js/tippy.all.min.js' },
+      { src: '/js/simplebar.min.js' },
+      { src: '/js/bootstrap-slider.min.js' },
+      { src: '/js/snackbar.js' },
+      { src: '/js/clipboard.min.js' },
+      { src: '/js/counterup.min.js' },
+      { src: '/js/magnific-popup.min.js' },
+      { src: '/js/slick.min.js' },
+      { src: '/js/custom.js' }  
+      ]
   },
 
   /*
@@ -26,13 +46,14 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [
-  ],
 
   /*
   ** Plugins to load before mounting the App
   */
+
   plugins: [
+
+
   ],
 
   /*
@@ -42,12 +63,13 @@ module.exports = {
     // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios',
     // Doc: https://bootstrap-vue.js.org/docs/
-    'bootstrap-vue/nuxt'
+    ['bootstrap-vue/nuxt', { css: false }],
   ],
   /*
   ** Axios module configuration
   */
   axios: {
+
     // See https://github.com/nuxt-community/axios-module#options
   },
 
@@ -58,8 +80,15 @@ module.exports = {
     /*
     ** You can extend webpack config here
     */
+    plugins: [
+      new webpack.ProvidePlugin({
+        '$': 'jquery',
+        'jQuery': 'jquery',
+        'window.jQuery': 'jquery'
+      })],
+    extractCSS: true,
     extend(config, ctx) {
-      
+
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,50 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-       Hellow New Macher 2!
-      </h1>
-      <h2 class="subtitle">
-        New Macher Jobs
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <span>Hello World!</span>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+//import jQuery from "jquery";
 
 export default {
-  components: {
-    Logo
+  methods: {
+    // initAutocomplete: function() {
+    //   var options = {
+    //     types: ["(cities)"]
+    //     // componentRestrictions: {country: "us"}
+    //   };
+
+    //   var input = document.getElementById("autocomplete-input");
+    //   var autocomplete = new google.maps.places.Autocomplete(input, options);
+    // }
+  },
+
+  mounted() {
+    if (process.browser) {
+      // jQuery(document).ready(function() {
+      //   console.log("hi");
+      // });
+    }
+
+    // this.initAutocomplete();
+    // // Autocomplete adjustment for homepage
+    // if ($(".intro-banner-search-form")[0]) {
+    //   setTimeout(function() {
+    //     $(".pac-container").prependTo(".intro-search-field.with-autocomplete");
+    //   }, 300);
+    // }
+
+    // // Snackbar for user status switcher
+    // $("#snackbar-user-status label").click(function() {
+    //   Snackbar.show({
+    //     text: "Your status has been changed!",
+    //     pos: "bottom-center",
+    //     showAction: false,
+    //     actionText: "Dismiss",
+    //     duration: 3000,
+    //     textColor: "#fff",
+    //     backgroundColor: "#383838"
+    //   });
+    // });
   }
-}
+};
 </script>
-
-<style>
-
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
